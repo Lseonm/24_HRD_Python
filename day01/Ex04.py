1f6cef8982ea142956f106d455f2706da62e88a3
Lseonm <151723449+Lseonm@users.noreply.github.com>
2024-09-05 16:23:08 +0900

    Delete day01 directory

--- day01/Ex04.py
+++
@@ -1,6 +0,0 @@
-x = int(input("정수값 입력 : "))
-
-if x > 0 :
-    print("양수")
-else :
-    print("음수")
--- day01/HelloWorld.py
+++
@@ -1,4 +0,0 @@
-# "Hello World" -> 메모리에 올라갔다가 사라짐. (안나옴)
-print("Hello World") # () 는 argument임 (arg)
-
-a : int = 1
